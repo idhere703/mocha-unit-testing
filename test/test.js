ae50ec24097075c3e0fbf3f4a4fb9249ba9568fe
Brandon <brandon@gooddonegreat.com>
2016-08-17 22:25:51 -0400

    Use of arrow functions is discouraged :<

--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
 var assert = require('assert');
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
+    });
+    it('Should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(1));
     });
   });
 });
